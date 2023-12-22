6489f082899d9f2e98a4145db08ab52a8dc9719d
UBnkantah <ubongnkantahuwem@gmail.com>
2023-12-22 02:09:36 +0000

    starter

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.use(
   express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
 );
 
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome")
+})
+
 app.use("/api/user", authRoute);
 
 app.listen(PORT, () => {
