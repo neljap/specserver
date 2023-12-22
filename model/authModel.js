9e1ec94aa33efcb0154485430e0560561be3ff39
UBnkantah <ubongnkantahuwem@gmail.com>
2023-12-22 23:19:12 +0000

    starter

--- model/authModel.js
+++ model/authModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  fullname: { type: String },
+  fullname: { type: String, default: "" },
   number: { type: String, default: "" },
   referId: { type: String, default: "" },
   password: { type: String, required: true },
