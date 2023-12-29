c30dee7daf293416ea78448dcd97a11013cab9bc
UBnkantah <ubongnkantahuwem@gmail.com>
2023-12-29 15:33:25 +0000

    update

--- index.js
+++ index.js
@@ -13,8 +13,12 @@ database();
 
 let app = express();
 
-
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 app.use(
   express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
